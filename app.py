fc79494f2c18b9ff7019a88f6ccf347302b9a289
Shravani Shinde <shravanis0511@gmail.com>
2026-08-24 21:56:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("Hello from Jenkins and GitHub!")
-print("File is modified.")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This application was deployed using Jenkins."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
